42bd36708b479cca69e989b9720bb77790dc6b0d
riicodes <itsrii.97@gmail.com>
2021-06-07 17:35:54 +0545

    sharing posts by email

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
+from django.core.mail import send_mail
 from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
+
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -7,7 +10,7 @@ class PostList(ListView):
     queryset = Post.published.all()
     template_name = 'blog/post/list.html'
     context_object_name = 'posts'
-    paginate_by = 1
+    paginate_by = 5
 
 
 def post_detail(request, year, month, day, post):
@@ -19,3 +22,24 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     return render(request, 'blog/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}"
+            send_mail(subject, message, 'itsrii.97@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html',
+                  {'post': post,
+                   'form': form,
+                   'sent': sent})
