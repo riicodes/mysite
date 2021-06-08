5d0f32a7b222f24d82b3f007558b0709a9440191
riicodes <itsrii.97@gmail.com>
2021-06-08 10:42:21 +0545

    similar posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Count
 
 from .forms import EmailPostForm, CommentForm
 from .models import Post
@@ -39,6 +40,10 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     new_comment = None
 
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     if request.method == 'POST':
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
@@ -51,7 +56,8 @@ def post_detail(request, year, month, day, post):
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
